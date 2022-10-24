ac04a697f5128d7b4950a2a06a4dc05d0c2bb109
Saad <magnite@hotmail.com>
2022-10-23 20:43:33 -0500

    Added a Main page Screen

---
+++ src/main/java/schedular/Controllers/MainPageController.java
@@ -0,0 +1,88 @@
+package schedular.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+
+public class MainPageController {
+    @FXML
+    private Button addApptButton;
+
+    @FXML
+    private TableView<?> apptTable;
+
+    @FXML
+    private Button deleteAppButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button modApptButton;
+
+    @FXML
+    private Button reportButton;
+
+    @FXML
+    private RadioButton viewAllRadio;
+
+    @FXML
+    private RadioButton viewCustomerRadio;
+
+    @FXML
+    private RadioButton viewMonthRadio;
+
+    @FXML
+    private RadioButton viewWeeklyRadio;
+
+    @FXML
+    private ToggleGroup viewsToggle;
+
+    @FXML
+    void addAppoint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteApptAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modifyAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportGen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewMonthly(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewWeekly(ActionEvent event) {
+
+    }
+}
